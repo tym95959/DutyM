b8b1e1f60396d1effaf077fe863ec8b023d9ad04
tym95959 <tym95959@gmail.com>
2026-01-29 13:05:40 +0500

    Update dcfire.js

--- dcfire.js
+++ dcfire.js
@@ -1,3 +1,9 @@
+// dcfire.js - Updated with messaging
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getMessaging, getToken, onMessage } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js';
+import { getFirestore, collection, addDoc, query, orderBy, onSnapshot } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+import { getAuth, signInAnonymously } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
+
 const firebaseConfig = {
   apiKey: "AIzaSyAf_sjwVHG65vKhezpS_L7KC2j0WHIDaWc",
   authDomain: "leelidc-1f753.firebaseapp.com",
@@ -8,7 +14,10 @@ const firebaseConfig = {
   measurementId: "G-3KD6ZYS599"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-// expose globally
-window.db = firebase.firestore();
+export { app, messaging, getToken, onMessage, db, collection, addDoc, query, orderBy, onSnapshot, auth, signInAnonymously };
