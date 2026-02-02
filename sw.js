24c14d80c89722b2edea00b09667c5ec0706579c
tym95959 <tym95959@gmail.com>
2026-02-02 20:23:56 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
-self.addEventListener("install", e => {
-  self.skipWaiting();
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAf_sjwVHG65vKhezpS_L7KC2j0WHIDaWc",
+  authDomain: "leelidc-1f753.firebaseapp.com",
+  projectId: "leelidc-1f753",
+  messagingSenderId: "43622932335",
+  appId: "1:43622932335:web:a7529bce1f19714687129a"
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon-192.png"
+  });
 });
