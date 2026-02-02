7e6ed53111a55eb6708ec13e4372c959cc366dc9
tym95959 <tym95959@gmail.com>
2026-02-02 20:07:42 +0500

    Update dcfire.js

--- dcfire.js
+++ dcfire.js
@@ -1,18 +1,15 @@
-<!-- dcfire.js -->
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAf_sjwVHG65vKhezpS_L7KC2j0WHIDaWc",
-    authDomain: "leelidc-1f753.firebaseapp.com",
-    projectId: "leelidc-1f753",
-    storageBucket: "leelidc-1f753.firebasestorage.app",
-    messagingSenderId: "43622932335",
-    appId: "1:43622932335:web:a7529bce1f19714687129a",
-    measurementId: "G-3KD6ZYS599"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyAf_sjwVHG65vKhezpS_L7KC2j0WHIDaWc",
+  authDomain: "leelidc-1f753.firebaseapp.com",
+  projectId: "leelidc-1f753",
+  storageBucket: "leelidc-1f753.firebasestorage.app",
+  messagingSenderId: "43622932335",
+  appId: "1:43622932335:web:a7529bce1f19714687129a",
+  measurementId: "G-3KD6ZYS599"
+};
 
-  export const app = initializeApp(firebaseConfig);
-  export const db = getFirestore(app);
-</script>
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
