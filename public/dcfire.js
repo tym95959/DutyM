455a9ac95a27313ee081c4a1e992ee6e8cd8ee7a
tym95959 <tym95959@gmail.com>
2026-02-02 23:23:34 +0500

    Update dcfire.js

--- public/dcfire.js
+++ public/dcfire.js
@@ -1,17 +1,17 @@
-// dcfire.js
+// Initialize Firebase app and exports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAf_sjwVHG65vKhezpS_L7KC2j0WHIDaWc",
   authDomain: "leelidc-1f753.firebaseapp.com",
   projectId: "leelidc-1f753",
   storageBucket: "leelidc-1f753.firebasestorage.app",
   messagingSenderId: "43622932335",
   appId: "1:43622932335:web:a7529bce1f19714687129a",
-  measurementId: "G-3KD6ZYS599"
+  measurementId: "G-3KD6ZYS599",
+  databaseURL: "https://leelidc-1f753-default-rtdb.firebaseio.com/"
 };
-
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
