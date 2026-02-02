c666b33cc8fa277fc4b694d24f37b596bb612be7
tym95959 <tym95959@gmail.com>
2026-02-02 23:20:43 +0500

    Delete api directory

--- api/save-token.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    // In production, save token to a database
-    console.log('Token received:', req.body.token);
-    res.status(200).json({ message: 'Token saved' });
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
-}
