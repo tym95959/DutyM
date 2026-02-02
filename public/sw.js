1acdf5829e579fe12449730f495d75ca7d228ee6
tym95959 <tym95959@gmail.com>
2026-02-02 22:04:36 +0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,24 @@
 const cacheName = "my-pwa-cache-v1";
-const assets = ["/", "/myapps.html", "/icon-192.png"];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(assets)));
+// Make sure all paths exist at the root
+const assets = [
+  "/",
+  "/myapps.html",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(assets))
+      .catch(err => {
+        console.error("Failed to cache assets:", err);
+      })
+  );
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
