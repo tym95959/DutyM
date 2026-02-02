74081c38756a50e544dcbdf0a68a3b8a2895ed73
tym95959 <tym95959@gmail.com>
2026-02-02 22:01:39 +0500

    Create dcfire.js

---
+++ public/dcfire.js
@@ -0,0 +1,16 @@
+// dcfire.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAf_sjwVHG65vKhezpS_L7KC2j0WHIDaWc",
+  authDomain: "leelidc-1f753.firebaseapp.com",
+  projectId: "leelidc-1f753",
+  storageBucket: "leelidc-1f753.firebasestorage.app",
+  messagingSenderId: "43622932335",
+  appId: "1:43622932335:web:a7529bce1f19714687129a",
+  measurementId: "G-3KD6ZYS599"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
