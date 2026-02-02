62e82e6dc353e9e934be6e3e9e403159afacfce8
tym95959 <tym95959@gmail.com>
2026-02-02 22:37:23 +0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,19 @@
+// functions/index.js
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.sendUserNotification = functions.firestore
   .document("users/{docId}")
-  .onCreate(async (snap, context)=>{
+  .onCreate(async (snap, context) => {
     const newUser = snap.data();
-    const tokensSnapshot = await admin.firestore().collection("tokens").get();
-    const tokens = tokensSnapshot.docs.map(doc=>doc.data().token);
-    if(tokens.length===0) return;
 
+    // Get all device tokens
+    const tokensSnapshot = await admin.firestore().collection("tokens").get();
+    const tokens = tokensSnapshot.docs.map(doc => doc.data().token);
+    if(tokens.length === 0) return;
+
+    // Send notification
     const message = {
       notification: {
         title: "New User Added",
@@ -18,6 +22,6 @@ exports.sendUserNotification = functions.firestore
       tokens: tokens
     };
 
-    await admin.messaging().sendMulticast(message);
-    console.log("Notifications sent:", tokens);
+    const response = await admin.messaging().sendMulticast(message);
+    console.log("Notifications sent:", response.successCount);
   });
