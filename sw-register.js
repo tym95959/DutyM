8a8dac4e434485b7c6a2a586d94ae9ff444b7a7a
tym95959 <tym95959@gmail.com>
2026-02-06 10:03:10 +0500

    Delete sw-register.js

--- sw-register.js
+++
@@ -1,6 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/firebase-messaging-sw.js')
-    .then(registration => {
-        console.log('Service Worker registered:', registration);
-    }).catch(err => console.log('SW registration failed:', err));
-}
