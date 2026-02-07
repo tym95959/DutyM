80c81a1979b38cf2d00586db129d1d52fc378664
tym95959 <tym95959@gmail.com>
2026-02-07 10:16:00 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Replace with your Firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyAf_sjwVHG65vKhezpS_L7KC2j0WHIDaWc",
+    authDomain: "leelidc-1f753.firebaseapp.com",
+    projectId: "leelidc-1f753",
+    storageBucket: "leelidc-1f753.firebasestorage.app",
+    messagingSenderId: "43622932335",
+    appId: "1:43622932335:web:a7529bce1f19714687129a",
+    measurementId: "G-3KD6ZYS599"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
