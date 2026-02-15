300b9dbba4a569fd147f0538d31f170025e64ba4
tym95959 <tym95959@gmail.com>
2026-02-15 20:07:37 +0500

    Add Co.js with company information

---
+++ Co.js
@@ -0,0 +1,9 @@
+// Co.js
+const companyInfo = {
+  name: "Koveli Lounge",
+  address: "Velana International Airport",
+  phone: "+960 304 6677"
+};
+// Make it available globally
+window.companyInfo = companyInfo;
+console.log("Co.js loaded:", companyInfo);
