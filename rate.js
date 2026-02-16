677a97aaa4ca971491aec97899fc6a14b18beaac
tym95959 <tym95959@gmail.com>
2026-02-16 09:45:54 +0500

    Create rate.js

---
+++ rate.js
@@ -0,0 +1,11 @@
+ // Rate pricing data
+        const rates = {
+            AdultAUSD: { price: 77.00, GST: 8, WP: "Leeli" },
+            KidsAUSD: { price: 55.00, GST: 8, WP: "Leeli" },
+            AdultAMVR: { price: 1187.34, GST: 8, WP: "Leeli" },
+            KidsAMVR: { price: 848.10, GST: 8, WP: "Leeli" },
+            AdultBUSD: { price: 55.00, GST: 8, WP: "Leeli" },
+            KidsBUSD: { price: 55.00, GST: 8, WP: "Leeli" },
+            AdultBMVR: { price: 848.10, GST: 8, WP: "Leeli" },
+            KidsBMVR: { price: 848.10, GST: 8, WP: "Leeli" }
+        };
