e4814d08caffbc4172e4610dce257e8acade6f5d
tym95959 <tym95959@gmail.com>
2026-02-26 10:18:25 +0500

    Update users.js

--- users.js
+++ users.js
@@ -1,5 +1,5 @@
 const users = [
-  { username: "Irufan", name: "Abdulla Irufan", passwordHash: "557808176cfefce664c3694042b720e1be6a41974527178598622edc21dc6d9d", Level: "Supervisor",  RCNo: "979"},
+  { username: "Irufan", name: "Abdulla Irufan", passwordHash: "557808176cfefce664c3694042b720e1be6a41974527178598622edc21dc6d9d", Level: "Admin",  RCNo: "979"},
   { username: "Hussain", name: "Hussain Ahmed", passwordHash: "0a041b9462caa4a31bac3567e0b6e6fd9100787db2ab433d96f6d178cabfce90", Level: "Supervisor", RCNo: "T-0115" },
   { username: "Mohamed", name: "Mohamed Ibrahim", passwordHash: "c75f28325cfa028ea13872f977a29e0e87c99a4f390fe260f24d7e1f05fb8d75",  Level: "Supervisor", RCNo: "623" },
   { username: "fiyaza", name: "Fiyaza Abdulla", passwordHash: "ca358cbdaddb39c71130153049e7bc96f387b1f0030deac24841c74f269831ed",  Level: "User", RCNo: "1081" },
